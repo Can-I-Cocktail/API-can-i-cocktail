85f8331efd1f68166dbcbbd3ffe7bcace4155e77
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-10 10:47:25 -0500

    connected Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import AppRoutes from "./AppRoutes";
+import Navbar from "../client/components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AppRoutes />
     </div>
   );
